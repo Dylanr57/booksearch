bd2b32d4dfca80474a92b7ae0abf70681b58970d
Dylanr57 <dylanr574423@gmail.com>
2019-02-07 20:45:23 -0800

    Updates

---
+++ precache-manifest.1b62c0db3a9eece823de9e725531b338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/booksearch/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "880255d7370d686752ee",
+    "url": "/booksearch/static/js/runtime~main.880255d7.js"
+  },
+  {
+    "revision": "4e3aeaf03ab27a7c2bee",
+    "url": "/booksearch/static/js/main.4e3aeaf0.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/booksearch/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "4e3aeaf03ab27a7c2bee",
+    "url": "/booksearch/static/css/main.9c27546e.chunk.css"
+  },
+  {
+    "revision": "3e5ac63a80e0ec9503801dd459902c26",
+    "url": "/booksearch/index.html"
+  }
+];
